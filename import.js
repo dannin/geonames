a61d20b323ddb4ffea7ba3591ff8676bfcc18cce
Dale Annin <dannin@blackberry.com>
2014-09-06 15:13:24 -0400

    fixup import on parsing

--- import.js
+++ import.js
@@ -2,15 +2,11 @@ var lineReader = require('line-reader');
 var database = require('database');
 
 console.log("Importing geonames data into mongo database. This may take a while.");
-lineReader.eachLine('./geonames-data/US.txt', function(line, last) {
+lineReader.eachLine('./geonames-data/CA.txt', function(line, last) {
 
 	var city = line.split('\t').structure();
 
-  if (city.is) { // if this is a city.
-
-  	// Delete un-needed object properties so we don't add useless info to DB.
-  	delete city.is;
-  	delete city.type;
+  if (city.city) { // if this is a city.
 
 		database.use_safe("geonames", "cities", function(db) {
 			db.save(city, { safe: true }, function(err, doc) {
@@ -46,20 +42,19 @@ lineReader.eachLine('./geonames-data/US.txt', function(line, last) {
 Array.prototype.structure = function() {
 	// from geonames readme: - P.PPL	populated place	a city, town, village, or other agglomeration of buildings where people live and work
 	return {
-		_id: Number(this[0]),
+		country: this[0],
+		postal: this[1],
+		city: this[2],
+		state: this[3],
+		state_code: this[4],
+		province: this[5],
+		province_code: this[6],
+		community: this[7],
+		community_code: this[8],
 
-		name: this[1],
-		state: this[10],
-		country: this[8],
-
-		loc: { 
+		loc: {
 			type: "Point",
-			coordinates: [Number(this[5]), Number(this[4])], // "All documents must store location data in the same order. If you use latitude and longitude as your coordinate system, always store longitude first. MongoDB’s 2d spherical index operators only recognize [ longitude, latitude] ordering."
-		},
-
-		timezone: this[17],
-
-		is: ((this[6] == "P") ? true : false), // if object type is city.
-		type: ((this[6] == "P") ? "city" : "other") // todo: add a parse function to parse other geonames db types
+			coordinates: [Number(this[10]), Number(this[9])], // "All documents must store location data in the same order. If you use latitude and longitude as your coordinate system, always store longitude first. MongoDB’s 2d spherical index operators only recognize [ longitude, latitude] ordering."
+		}
 	}
 }
